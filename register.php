a2a500f08653a291687fcef7dd103e1da13b4bf1
DIMAS2Z <idimasprayogo123@gmail.com>
2024-12-06 11:44:19 +0700

    Update register.php

--- register.php
+++ register.php
@@ -79,7 +79,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Register</title>
-    <link rel="stylesheet" href="style.css"> <!-- Jika Anda memiliki file CSS -->
+    <link rel="stylesheet" href="style.css"> 
 </head>
 <body>
     <div class="container">
